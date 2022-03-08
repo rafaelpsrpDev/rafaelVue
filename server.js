cd90de78ba07d657f292d83db21f9fc64e49fc76
rafaelpsrpDev <rafaelpsrp@outlook.com>
2022-03-08 17:04:01 +0000

    alterações com o dist de production

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const porta = process.env.PORT || 8080
+
+app.use(express.static(__dirname + '/dist/'));
+
+app.get('/', function(req, res){
+    res.sendfile(__dirname + '/dist/index.html');
+});
+
+app.listen(porta, function(){
+    console.log(`CHEGOU `);
+})
